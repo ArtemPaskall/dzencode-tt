fc80c5a9f183f91413d4bce0454704c8537dd828
ArtemPaskall <artempasskal@gmail.com>
2026-03-11 12:45:20 +0200

    first commit, add multilang

---
+++ app/not-found.tsx
@@ -0,0 +1,33 @@
+import { Montserrat } from "next/font/google";
+import { getTranslations } from "next-intl/server";
+import Link from "next/link";
+import st from "./styles/not-found.module.scss";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-montserrat",
+});
+
+export async function generateMetadata() {
+  const t = await getTranslations("NotFoundPage");
+  return {
+    title: t("title"),
+  };
+}
+
+export default async function NotFound() {
+  const t = await getTranslations("NotFoundPage");
+
+  return (
+    <div className={montserrat.className + " " + st.notFound}>
+      <h1 className={st.notFound__title}>{t("title")}</h1>
+      <p className={st.notFound__description}>{t("description")}</p>
+      <Link href={`/`}>
+        <button type="button" className={st["notFound__button"]}>
+          {t("goHome")}
+        </button>
+      </Link>
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,19 @@
+// next.config.ts
+
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
