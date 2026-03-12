6855e98e3c53bd47e3b39063b0a5d794cd6c301f
ArtemPaskall <artempasskal@gmail.com>
2026-03-12 21:42:06 +0200

    allow cors

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,21 @@
-// next.config.ts
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
+  experimental: {
+    // TS поки що не знає про allowedDevOrigins
+    allowedDevOrigins: ['https://dzencode-tt-production.up.railway.app'] as any,
+  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
       },
     ],
   },
-};
+}
 
-export default withNextIntl(nextConfig);
+export default withNextIntl(nextConfig)
