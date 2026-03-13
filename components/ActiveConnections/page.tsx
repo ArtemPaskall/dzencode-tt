33ad87d73fa0b38a0a770705ad66139e006286b3
ArtemPaskall <artempasskal@gmail.com>
2026-03-13 18:59:49 +0200

    receive only checked connections

--- components/ActiveConnections/page.tsx
+++ components/ActiveConnections/page.tsx
@@ -1,3 +1,37 @@
+// 'use client'
+
+// import { useEffect, useState } from 'react'
+// import { getSocket } from '@/libs/socket'
+// import st from './activeConnections.module.scss'
+// import { useTranslations } from 'next-intl'
+
+// export default function ActiveConnections() {
+//   const t = useTranslations('ActiveConnections')
+//   const [users, setUsers] = useState<number | null>(null)
+
+//   useEffect(() => {
+//     const socket = getSocket()
+
+//     const handler = (count: number) => setUsers(count)
+
+//     socket.on('users', handler)
+
+//     socket.emit('request-users')
+
+//     return () => {
+//       socket.off('users', handler)
+//     }
+//   }, [])
+
+//   return (
+//     <div className={st.activeConnections}>
+//       <div className={st.activeConnections__users}> {users ?? '...'}</div>
+//       <div className={st.activeConnections__connections}>
+//         {t('Connections')}
+//       </div>
+//     </div>
+//   )
+// }
 'use client'
 
 import { useEffect, useState } from 'react'
@@ -8,24 +42,30 @@ import { useTranslations } from 'next-intl'
 export default function ActiveConnections() {
   const t = useTranslations('ActiveConnections')
   const [users, setUsers] = useState<number | null>(null)
+  const [initialized, setInitialized] = useState(false)
 
   useEffect(() => {
     const socket = getSocket()
 
-    const handler = (count: number) => setUsers(count)
+    const handler = (count: number) => {
+      if (!initialized) setInitialized(true) // перше реальне повідомлення
+      setUsers(count)
+    }
 
     socket.on('users', handler)
 
-    socket.emit('request-users')
+    socket.emit('request-users') // запитати кількість
 
     return () => {
       socket.off('users', handler)
     }
-  }, [])
+  }, [initialized])
 
   return (
     <div className={st.activeConnections}>
-      <div className={st.activeConnections__users}> {users ?? '...'}</div>
+      <div className={st.activeConnections__users}>
+        {initialized ? users : '...'}
+      </div>
       <div className={st.activeConnections__connections}>
         {t('Connections')}
       </div>
