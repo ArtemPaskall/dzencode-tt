e02af1e4ac0030b32a51262892b41b90eaecc14a
ArtemPaskall <artempasskal@gmail.com>
2026-03-15 15:05:29 +0200

    fix bug

--- app/[locale]/(root)/products/page.tsx
+++ app/[locale]/(root)/products/page.tsx
@@ -1,43 +1,8 @@
-import { getTranslations } from 'next-intl/server'
-import '@/app/styles/globals.scss'
-import st from './products.module.scss'
-import { Link } from '@/i18n/navigation'
-import { Product } from '@/types'
-
-export async function generateMetadata() {
-  const t = await getTranslations('Products')
-  return { title: t('title') }
-}
-
-export default async function Products() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/products`, {
-    cache: 'no-store',
-  })
-
-  if (!res.ok) return <div>Failed to load products ({res.status})</div>
-
-  const products: Product[] = await res.json()
-
-  return (
-    <div className={st.productsPage}>
-      <Link href="/products/add-product">Add Product</Link>
-      <div className={st.productsList}>
-        {products.map((product) => (
-          <div key={product.serial_number} className={st.productItem}>
-            {product.title}
-          </div>
-        ))}
-      </div>
-    </div>
-  )
-}
 // import { getTranslations } from 'next-intl/server'
 // import '@/app/styles/globals.scss'
 // import st from './products.module.scss'
 // import { Link } from '@/i18n/navigation'
 // import { Product } from '@/types'
-// import db from '@/libs/db'
-// import { RowDataPacket } from 'mysql2'
 
 // export async function generateMetadata() {
 //   const t = await getTranslations('Products')
@@ -45,22 +10,57 @@ export default async function Products() {
 // }
 
 // export default async function Products() {
-//   const [rows] = await db.query<RowDataPacket[]>('SELECT * FROM products')
-//   const products: Product[] = rows as Product[]
-//   console.log(products)
+//   const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/products`, {
+//     cache: 'no-store',
+//   })
+
+//   if (!res.ok) return <div>Failed to load products ({res.status})</div>
+
+//   const products: Product[] = await res.json()
 
 //   return (
 //     <div className={st.productsPage}>
 //       <Link href="/products/add-product">Add Product</Link>
-
 //       <div className={st.productsList}>
 //         {products.map((product) => (
 //           <div key={product.serial_number} className={st.productItem}>
-//             <div>{product.title}</div>
-//             <div>{product.guarantee_start.toString()}</div>
+//             {product.title}
 //           </div>
 //         ))}
 //       </div>
 //     </div>
 //   )
 // }
+import { getTranslations } from 'next-intl/server'
+import '@/app/styles/globals.scss'
+import st from './products.module.scss'
+import { Link } from '@/i18n/navigation'
+import { Product } from '@/types'
+import db from '@/libs/db'
+import { RowDataPacket } from 'mysql2'
+
+export async function generateMetadata() {
+  const t = await getTranslations('Products')
+  return { title: t('title') }
+}
+
+export default async function Products() {
+  const [rows] = await db.query<RowDataPacket[]>('SELECT * FROM products')
+  const products: Product[] = rows as Product[]
+  console.log(products)
+
+  return (
+    <div className={st.productsPage}>
+      <Link href="/products/add-product">Add Product</Link>
+
+      <div className={st.productsList}>
+        {products.map((product) => (
+          <div key={product.serial_number} className={st.productItem}>
+            <div>{product.title}</div>
+            <div>{product.guarantee_start.toString()}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
