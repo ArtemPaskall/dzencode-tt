7060d777ed6d6cd1fe57079c1d96d422a92791bf
ArtemPaskall <artempasskal@gmail.com>
2026-03-20 18:09:19 +0200

    add list empty message

--- app/[locale]/(root)/orders/OrdersClient.tsx
+++ app/[locale]/(root)/orders/OrdersClient.tsx
@@ -276,25 +276,32 @@ export default function OrdersClient({ initialOrders }: Props) {
 
               {isProdListOpen && (
                 <div className={st.addProd__prodList}>
-                  {availableProducts.map((product) => (
-                    <div key={product.id}>
-                      <label className={st.addProd__label}>
-                        <input
-                          className={st.addProd__checkbox}
-                          type="checkbox"
-                          checked={selectedProducts.includes(product.id)}
-                          onChange={() => toggleProduct(product.id)}
-                        />
-                        {product.title}
-                      </label>
-                    </div>
-                  ))}
-                  <div
-                    className={st.addProd__addProdButton}
-                    onClick={handleAddProductsToOrder}
-                  >
-                    {t('add')}
-                  </div>
+                  {availableProducts.length === 0 ? (
+                    <div className={st.addProd__empty}>{t('emptyList')}</div>
+                  ) : (
+                    <>
+                      {availableProducts.map((product) => (
+                        <div key={product.id}>
+                          <label className={st.addProd__label}>
+                            <input
+                              className={st.addProd__checkbox}
+                              type="checkbox"
+                              checked={selectedProducts.includes(product.id)}
+                              onChange={() => toggleProduct(product.id)}
+                            />
+                            {product.title}
+                          </label>
+                        </div>
+                      ))}
+
+                      <div
+                        className={st.addProd__addProdButton}
+                        onClick={handleAddProductsToOrder}
+                      >
+                        {t('add')}
+                      </div>
+                    </>
+                  )}
                 </div>
               )}
 
--- app/api/orders/route.ts
+++ app/api/orders/route.ts
@@ -1,35 +1,71 @@
 import { NextResponse } from 'next/server'
 import db from '@/libs/db'
+import { Order, OrderWithProductRow, Product } from '@/types'
 
 export async function GET() {
   try {
-    const [rows] = await db.query(`
+    const [rows] = await db.query<OrderWithProductRow[]>(`
       SELECT 
-        o.id as orderId,
-        o.title as orderTitle,
-        p.id as productId,
-        p.title as productTitle
+        o.id as order_id,
+        o.title as order_title,
+        o.description as order_description,
+        o.date as order_date,
+
+        p.id as product_id,
+        p.serial_number,
+        p.is_new,
+        p.photo,
+        p.title as product_title,
+        p.type,
+        p.specification,
+        p.guarantee_start,
+        p.guarantee_end,
+        p.price,
+        p.date as product_date
+
       FROM orders o
       LEFT JOIN order_products op ON op.order_id = o.id
       LEFT JOIN products p ON p.id = op.product_id
     `)
 
-    const ordersMap = new Map()
+    const ordersMap = new Map<number, Order>()
 
-    for (const row of rows as any[]) {
-      if (!ordersMap.has(row.orderId)) {
-        ordersMap.set(row.orderId, {
-          id: row.orderId,
-          title: row.orderTitle,
+    for (const row of rows) {
+      if (!ordersMap.has(row.order_id)) {
+        ordersMap.set(row.order_id, {
+          id: row.order_id,
+          title: row.order_title,
+          description: row.order_description,
+          date:
+            row.order_date instanceof Date
+              ? row.order_date.toISOString()
+              : row.order_date,
           products: [],
         })
       }
 
-      if (row.productId) {
-        ordersMap.get(row.orderId).products.push({
-          id: row.productId,
-          title: row.productTitle,
-        })
+      if (row.product_id) {
+        const product: Product = {
+          id: row.product_id,
+          serial_number: row.serial_number!,
+          is_new: Boolean(row.is_new),
+          photo: row.photo!,
+          title: row.product_title!,
+          type: row.type!,
+          specification: row.specification!,
+          guarantee_start: row.guarantee_start
+            ? new Date(row.guarantee_start).toISOString()
+            : '',
+          guarantee_end: row.guarantee_end
+            ? new Date(row.guarantee_end).toISOString()
+            : '',
+          price: row.price ? JSON.parse(row.price) : [],
+          date: row.product_date
+            ? new Date(row.product_date).toISOString()
+            : '',
+        }
+
+        ordersMap.get(row.order_id)!.products.push(product)
       }
     }
 
